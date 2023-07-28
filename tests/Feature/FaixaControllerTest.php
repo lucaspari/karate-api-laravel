db13c128866d950582c2db458fa3b7fcfecba2e9
lucaspari <lucaspariii@gmail.com>
2023-07-28 11:05:06 -0300

    feat: added more tests to faixaController

--- tests/Feature/FaixaControllerTest.php
+++ tests/Feature/FaixaControllerTest.php
@@ -25,14 +25,29 @@ class FaixaControllerTest extends TestCase
     public function test_can_get_faixa_by_id()
     {
         // Create a Faixa record in the database
-        $faixa = Faixa::factory()->count(10)->create();
-        dd(strval($faixa));
+        $faixa = Faixa::factory()->createOne();
         // Make a GET request to the /api/faixas/{id} endpoint with the Faixa ID
         $response = $this->getJson('/api/faixas/' . $faixa->id);
-      
-
         // Assert that the response status is 200 OK
         $response->assertStatus(Response::HTTP_OK);
+        $response->assertJsonFragment([
+            "id" => $faixa->id,
+            "nome" => $faixa->nome,
+            "urlPath" => $faixa->urlPath
+        ]);
 
+    }
+    public function test_returns_404_for_invalid_faixa_id()
+    {
+        // Make a GET request to the /api/faixas/{id} endpoint with an invalid Faixa ID
+        $response = $this->getJson('/api/faixas/999');
+
+        // Assert that the response status is 404 Not Found
+        $response->assertStatus(Response::HTTP_NOT_FOUND);
+
+        // Assert that the response contains the expected error message
+        $response->assertJsonFragment([
+            'message' => 'Faixa não encontrada!',
+        ]);
     }
 }
